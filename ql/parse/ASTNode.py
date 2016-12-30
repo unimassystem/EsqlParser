318d20f35f631e80c7d1b043c05fc63109515c72
subway1211 <unimas@localhost.localdomain>
2016-12-30 15:15:41 +0800

    EsqlParser

---
+++ ql/parse/ASTNode.py
@@ -0,0 +1,46 @@
+'''
+Created on Dec 26, 2016
+
+@author: unimas
+'''
+
+
+
+
+class ASTNode(object):
+    __slots__ = ('tokType','tokValue','children')
+    
+    def __init__(self,tokType,tokValue,children):
+        self.tokType = tokType
+        self.tokValue = tokValue
+        self.children = children
+
+    def setTokType(self,tokType):
+        self.tokType = tokType
+        
+    def getTokType(self):
+        return self.tokType
+    
+    def getTokValue(self):
+        return self.tokValue
+        
+    def getChildren(self):
+        return self.children
+
+    def appendChildren(self,val):
+        return self.children.append(val)
+    
+    def toStringTree(self,depth=0):
+        tab = ''
+        for i in range(depth):
+            i = i
+            tab += '\t'
+        print( tab + '('+ self.tokType.name)
+        if self.tokValue != None:
+            print( tab + '\t'+ self.tokValue)
+        if(self.getChildren() != None):
+            depth += 1
+            for node in self.getChildren():
+                node.toStringTree(depth)
+        print(tab + ')')
+
